f97038d1cccaddff198bdcaa5af5ba5dd6accde3
markwkm <devnull@localhost>
2007-04-18 20:11:48 +0000

    [project @ 1840] Added the '-g' flag to compile everything with symbols for debuggin and oprofile.
    Added an --enable-debug flag to configure to compile with a DEBUG
    symbol.
    
    Created C stored functions for the Broker Volume and Customer Position
    transactions.

---
+++ code/storedproc/pgsql/c/broker_volume.c
@@ -0,0 +1,264 @@
+/*
+ * This file is released under the terms of the Artistic License.  Please see
+ * the file LICENSE, included in this package, for details.
+ *
+ * Copyright (C) 2007 Mark Wong
+ *
+ * Based on TPC-E Standard Specification Revision 1.0
+ */
+
+#include <sys/types.h>
+#include <unistd.h>
+#include <postgres.h>
+#include <fmgr.h>
+#include <executor/spi.h> /* this should include most necessary APIs */
+#include <executor/executor.h>  /* for GetAttributeByName() */
+#include <funcapi.h> /* for returning set of rows in order_status */
+#include <utils/lsyscache.h>
+
+/*
+ * FIXME
+ * This is part of the FROM clause and the WHERE clause from the 1.0.0 spec.
+ * Why doesn't it match the database?
+...
+		"         SECURITY,\n" \
+		"         CUSTOMER_ACCOUNT\n" \
+...
+		"WHERE    TR_B_ID = B_ID\n" \
+		"         AND TR_S_SYMB = S_SYMB\n" \
+		"         AND S_CO_ID = CO_ID\n" \
+		"         AND CO_IN_ID = IN_ID\n" \
+		"         AND SC_ID = IN_SC_ID\n" \
+		"         AND B_NAME IN (%s)\n" \
+		"         AND SC_NAME = %s\n" \
+...
+*/
+#define BVF1_1 \
+		"SELECT   B_NAME,\n" \
+		"         SUM(TR_QTY * TR_BID_PRICE)\n" \
+		"FROM     TRADE_REQUEST,\n" \
+		"         SECTOR,\n" \
+		"         INDUSTRY,\n" \
+		"         COMPANY,\n" \
+		"         BROKER,\n" \
+		"         SECURITY,\n" \
+		"         CUSTOMER_ACCOUNT\n" \
+		"WHERE    TR_CA_ID = CA_ID\n" \
+		"         AND CA_B_ID = B_ID\n" \
+		"         AND TR_S_SYMB = S_SYMB\n" \
+		"         AND S_CO_ID = CO_ID\n" \
+		"         AND CO_IN_ID = IN_ID\n" \
+		"         AND SC_ID = IN_SC_ID\n" \
+		"         AND B_NAME IN (%s)\n" \
+		"         AND SC_NAME = '%s'\n" \
+		"GROUP BY B_NAME\n" \
+		"ORDER BY 2 DESC;"
+
+#ifdef PG_MODULE_MAGIC
+PG_MODULE_MAGIC;
+#endif
+
+/* Prototypes to prevent potential gcc warnings. */
+Datum BrokerVolumeFrame1(PG_FUNCTION_ARGS);
+
+PG_FUNCTION_INFO_V1(BrokerVolumeFrame1);
+
+Datum BrokerVolumeFrame1(PG_FUNCTION_ARGS)
+{
+	FuncCallContext *funcctx;
+	AttInMetadata *attinmeta;
+	int call_cntr;
+	int max_calls;
+
+	int i;
+
+	int ndim, nitems;
+	int *dim;
+	char *p;	
+
+	char **values = NULL;
+
+	/* Stuff done only on the first call of the function. */
+	if (SRF_IS_FIRSTCALL()) {
+		MemoryContext oldcontext;
+
+		ArrayType *broker_list = PG_GETARG_ARRAYTYPE_P(0);
+		char *sector_name = (char *) PG_GETARG_TEXT_P(1);
+
+		int16 typlen;
+		bool typbyval;
+		char typalign;
+
+		int ret;
+		TupleDesc tupdesc;
+		SPITupleTable *tuptable = NULL;
+		HeapTuple tuple = NULL;
+
+		char sql[2048];
+		char bl[2048];
+
+		/*
+		 * Prepare a values array for building the returned tuple.
+		 * This should be an array of C strings, which will
+		 * be processed later by the type input functions.
+		 */
+		values = (char **) palloc(sizeof(char *) * 4);
+		values[1] = (char *) palloc(3 * sizeof(char));
+		values[2] = (char *) palloc(2 * sizeof(char));
+
+		/*
+		 * This might be overkill since we always expect single dimensions
+		 * arrays.
+		 */
+		ndim = ARR_NDIM(broker_list);
+		dim = ARR_DIMS(broker_list);
+		nitems = ArrayGetNItems(ndim, dim);
+		get_typlenbyvalalign(ARR_ELEMTYPE(broker_list), &typlen, &typbyval,
+				&typalign);
+		p = ARR_DATA_PTR(broker_list);
+		if (nitems > 0) {
+#ifdef DEBUG
+			elog(NOTICE, "[0.1] %s",
+					DatumGetCString(DirectFunctionCall1(textout,
+					PointerGetDatum(p))));
+#endif
+			strcat(bl, "'");
+			strcat(bl, DatumGetCString(DirectFunctionCall1(textout,
+					PointerGetDatum(p))));
+			strcat(bl, "'");
+			p = att_addlength(p, typlen, PointerGetDatum(p));                   
+			p = (char *) att_align(p, typalign);
+		}
+		for (i = 1; i < nitems; i++) {
+#ifdef DEBUG
+			elog(NOTICE, "[0.%d] %s", i,
+					DatumGetCString(DirectFunctionCall1(textout,
+					PointerGetDatum(p))));
+#endif
+			strcat(bl, ",'");
+			strcat(bl, DatumGetCString(DirectFunctionCall1(textout,
+					PointerGetDatum(p))));
+			strcat(bl, "'");
+			p = att_addlength(p, typlen, PointerGetDatum(p));                   
+			p = (char *) att_align(p, typalign);
+		}
+#ifdef DEBUG
+		elog(NOTICE, "[1] %s", DatumGetCString(DirectFunctionCall1(textout,
+				PointerGetDatum(sector_name))));
+#endif
+
+		/* create a function context for cross-call persistence */
+		funcctx = SRF_FIRSTCALL_INIT();
+
+		/* switch to memory context appropriate for multiple function calls */
+		oldcontext = MemoryContextSwitchTo(funcctx->multi_call_memory_ctx);
+
+		SPI_connect();
+
+		sprintf(sql, BVF1_1, bl, DatumGetCString(DirectFunctionCall1(textout,
+				PointerGetDatum(sector_name))));
+#ifdef DEBUG
+		elog(NOTICE, "SQL\n%s", sql);
+#endif /* DEBUG */
+		ret = SPI_exec(sql, 0);
+		if (ret == SPI_OK_SELECT) {
+			tupdesc = SPI_tuptable->tupdesc;
+			tuptable = SPI_tuptable;
+			tuple = tuptable->vals[0];
+		} else {
+			elog(NOTICE, "ERROR: sql not ok");
+		}
+
+		sprintf(values[1], "%d", SPI_processed);
+		/* FIXME: How do se we status properly? */
+		sprintf(values[2], "%d", 1);
+
+		if (SPI_processed == 0) {
+			values[0] = (char *) palloc(3 * sizeof(char));
+			values[3] = (char *) palloc(3 * sizeof(char));
+			strcpy(values[0], "{}");
+			strcpy(values[3], "{}");
+		} else {
+			values[0] = (char *) palloc((100 * SPI_processed + SPI_processed +
+					2) * sizeof(char));
+			values[3] = (char *) palloc(17 * SPI_processed + SPI_processed +
+					2 * sizeof(char));
+
+			strcpy(values[0], "{");
+			strcpy(values[3], "{");
+
+			if (SPI_processed > 0) {
+				strcat(values[0], SPI_getvalue(tuple, tupdesc, 1));
+				strcat(values[3], SPI_getvalue(tuple, tupdesc, 2));
+			}
+			for (i = 1; i < SPI_processed; i++) {
+				tuple = tuptable->vals[i];
+				strcat(values[0], ",");
+				strcat(values[0], SPI_getvalue(tuple, tupdesc, 1));
+
+				strcat(values[3], ",");
+				strcat(values[3], SPI_getvalue(tuple, tupdesc, 2));
+			}
+			strcat(values[0], "}");
+			strcat(values[3], "}");
+		}
+
+		/* total number of tuples to be returned */
+		funcctx->max_calls = 1;
+
+		/* Build a tuple descriptor for our result type */
+		if (get_call_result_type(fcinfo, NULL, &tupdesc) !=
+				TYPEFUNC_COMPOSITE) {
+			ereport(ERROR,
+					(errcode(ERRCODE_FEATURE_NOT_SUPPORTED),
+					errmsg("function returning record called in context "
+							"that cannot accept type record")));
+		}
+
+		/*
+		 * generate attribute metadata needed later to produce tuples from raw
+		 * C strings
+		 */
+		attinmeta = TupleDescGetAttInMetadata(tupdesc);
+		funcctx->attinmeta = attinmeta;
+
+		MemoryContextSwitchTo(oldcontext);
+	}
+
+	/* stuff done on every call of the function */
+	funcctx = SRF_PERCALL_SETUP();
+
+	call_cntr = funcctx->call_cntr;
+	max_calls = funcctx->max_calls;
+	attinmeta = funcctx->attinmeta;
+
+	if (call_cntr < max_calls) {
+		/* do when there is more left to send */
+		HeapTuple tuple;
+		Datum result;
+
+#ifdef DEBUG                                                                    
+		for (i = 0; i < 4; i++) {
+			elog(NOTICE, "%d %s", i, values[i]);
+		}
+#endif /* DEBUG */
+
+		/* Build a tuple. */
+		tuple = BuildTupleFromCStrings(attinmeta, values);
+
+		/* Make the tuple into a datum. */
+		result = HeapTupleGetDatum(tuple);
+
+		/* Clean up. */
+		for (i = 0; i < 4; i++) {                                               
+			pfree(values[i]);                                                   
+		} 
+		pfree(values);                                                   
+
+		SRF_RETURN_NEXT(funcctx, result);
+	} else {
+		/* Do when there is no more left. */
+		SPI_finish();
+		SRF_RETURN_DONE(funcctx);
+	}
+}
---
+++ code/storedproc/pgsql/c/customer_position.c
@@ -0,0 +1,513 @@
+/*
+ * This file is released under the terms of the Artistic License.  Please see
+ * the file LICENSE, included in this package, for details.
+ *
+ * Copyright (C) 2007 Mark Wong
+ *
+ * Based on TPC-E Standard Specification Revision 1.0
+ */
+
+#include <sys/types.h>
+#include <unistd.h>
+#include <postgres.h>
+#include <fmgr.h>
+#include <executor/spi.h> /* this should include most necessary APIs */
+#include <executor/executor.h>  /* for GetAttributeByName() */
+#include <funcapi.h> /* for returning set of rows in order_status */
+
+#ifdef PG_MODULE_MAGIC
+PG_MODULE_MAGIC;
+#endif
+
+#define CPF1_1 \
+		"SELECT c_id\n" \
+		"FROM   customer\n" \
+		"WHERE  c_tax_id = '%s';"
+
+#define CPF1_2 \
+		"SELECT c_st_id,\n" \
+		"       c_l_name,\n" \
+		"       c_f_name,\n" \
+		"       c_m_name,\n" \
+		"       c_gndr,\n" \
+		"       c_tier,\n" \
+		"       c_dob,\n" \
+		"       c_ad_id,\n" \
+		"       c_ctry_1,\n" \
+		"       c_area_1,\n" \
+		"       c_local_1,\n" \
+		"       c_ext_1,\n" \
+		"       c_ctry_2,\n" \
+		"       c_area_2,\n" \
+		"       c_local_2,\n" \
+		"       c_ext_2,\n" \
+		"       c_ctry_3,\n" \
+		"       c_area_3,\n" \
+		"       c_local_3,\n" \
+		"       c_ext_3,\n" \
+		"       c_email_1,\n" \
+		"       c_email_2\n" \
+		"FROM   customer\n" \
+		"WHERE  c_id = %ld;"
+
+#define CPF1_3 \
+		"SELECT   ca_id,\n" \
+		"         ca_bal,\n" \
+		"         SUM(hs_qty * lt_price) AS soma\n" \
+		"FROM     customer_account\n" \
+		"         LEFT OUTER JOIN holding_summary\n" \
+		"           ON hs_ca_id = ca_id,\n" \
+		"         last_trade\n" \
+		"WHERE    ca_c_id = %ld\n" \
+		"         AND lt_s_symb = hs_s_symb\n" \
+		"GROUP BY ca_id,ca_bal\n" \
+		"ORDER BY 3 ASC\n" \
+		"LIMIT 10;"
+
+#define CPF2_1 \
+		"SELECT   t_id,\n" \
+		"         t_s_symb,\n" \
+		"         t_qty,\n" \
+		"         st_name,\n" \
+		"         th_dts\n" \
+		"FROM     (SELECT   t_id AS id\n" \
+		"          FROM     trade\n" \
+		"          WHERE    t_ca_id = %ld\n" \
+		"          ORDER BY t_dts DESC\n" \
+		"          LIMIT 10) AS t,\n" \
+		"         trade,\n" \
+		"         trade_history,\n" \
+		"         status_type\n" \
+		"WHERE    t_id = id\n" \
+		"         AND th_t_id = t_id\n" \
+		"         AND st_id = th_st_id\n" \
+		"ORDER BY th_dts DESC\n" \
+		"LIMIT 30;"
+
+/* Prototypes to prevent potential gcc warnings. */
+Datum CustomerPositionFrame1(PG_FUNCTION_ARGS);
+Datum CustomerPositionFrame2(PG_FUNCTION_ARGS);
+
+PG_FUNCTION_INFO_V1(CustomerPositionFrame1);
+
+Datum CustomerPositionFrame1(PG_FUNCTION_ARGS)
+{
+	FuncCallContext *funcctx;
+	int i;
+	char **values = NULL;
+
+	int64 cust_id;
+
+	/* stuff done only on the first call of the function */
+	if (SRF_IS_FIRSTCALL()) {
+		MemoryContext oldcontext;
+
+		char tax_id[21];
+		text *tax_id_raw;
+
+		int ret;
+		char sql[1024];
+
+		TupleDesc tupdesc;
+		SPITupleTable *tuptable = NULL;
+		HeapTuple tuple;
+
+		/*
+		 * Prepare a values array for building the returned tuple.
+		 * This should be an array of C strings, which will
+		 * be processed later by the type input functions.
+		 */
+		values = (char **) palloc(sizeof(char *) * 23);
+		values[0] = (char *) palloc(12 * sizeof(char));
+		values[1] = (char *) palloc(132 * sizeof(char));
+		values[2] = (char *) palloc(3 * sizeof(char));
+		values[3] = (char *) palloc(192 * sizeof(char));
+		values[4] = (char *) palloc(12 * sizeof(char));
+		values[5] = (char *) palloc(4 * sizeof(char));
+		values[6] = (char *) palloc(4 * sizeof(char));
+		values[7] = (char *) palloc(4 * sizeof(char));
+		values[8] = (char *) palloc(4 * sizeof(char));
+		values[9] = (char *) palloc(4 * sizeof(char));
+		values[10] = (char *) palloc(4 * sizeof(char));
+		values[11] = (char *) palloc(20 * sizeof(char));
+		values[12] = (char *) palloc(51 * sizeof(char));
+		values[13] = (char *) palloc(51 * sizeof(char));
+		values[14] = (char *) palloc(6 * sizeof(char));
+		values[15] = (char *) palloc(6 * sizeof(char));
+		values[16] = (char *) palloc(6 * sizeof(char));
+		values[17] = (char *) palloc(31 * sizeof(char));
+		values[18] = (char *) palloc(2 * sizeof(char));
+		values[19] = (char *) palloc(31 * sizeof(char));
+		values[20] = (char *) palloc(11 * sizeof(char));
+		values[21] = (char *) palloc(11 * sizeof(char));
+		values[22] = (char *) palloc(11 * sizeof(char));
+		values[23] = (char *) palloc(2 * sizeof(char));
+
+		/*
+		 * I think this is the fastest way to initialize the strings, as
+		 * opposed to zeroing out the entire chunk of memory since palloc()
+		 * doesn't zero out the memory for us.
+		 */
+		for (i = 0; i < 24; i++) {
+			values[i][0] = '\0';
+		}
+
+		/* Create a function context for cross-call persistence. */
+		funcctx = SRF_FIRSTCALL_INIT();
+
+		/* Switch to memory context appropriate for multiple function calls. */
+		oldcontext = MemoryContextSwitchTo(funcctx->multi_call_memory_ctx);
+
+		cust_id = PG_GETARG_INT64(0);
+		tax_id_raw = PG_GETARG_TEXT_P(1);
+		strcpy(tax_id, DatumGetCString(DirectFunctionCall1(textout,
+				PointerGetDatum(tax_id_raw))));
+#ifdef DEBUG
+		elog(NOTICE, "[1] %ld", cust_id);
+		elog(NOTICE, "[2] %s", tax_id);
+#endif /* DEBUG */
+
+		SPI_connect();
+		if (cust_id == 0) {
+			sprintf(sql, CPF1_1, tax_id);
+#ifdef DEBUG
+			elog(NOTICE, "SQL\n%s", sql);
+#endif /* DEBUG */
+			ret = SPI_exec(sql, 0);
+			if (ret == SPI_OK_SELECT && SPI_processed > 0) {
+				tupdesc = SPI_tuptable->tupdesc;
+				tuptable = SPI_tuptable;
+				tuple = tuptable->vals[0];
+				cust_id = atoll(SPI_getvalue(tuple, tupdesc, 1));
+#ifdef DEBUG
+				elog(NOTICE, "Got cust_id ok: %ld", cust_id);
+#endif /* DEBUG */
+			} else {
+				elog(NOTICE, "ERROR: did not get cust_id.");
+			}
+		}
+
+		sprintf(sql, CPF1_2, cust_id);
+#ifdef DEBUG
+		elog(NOTICE, "SQL\n%s", sql);
+#endif /* DEBUG */
+		ret = SPI_exec(sql, 0);
+#ifdef DEBUG
+		elog(NOTICE, "%d row(s) returned.", SPI_processed);
+#endif /* DEBUG */
+		if (ret == SPI_OK_SELECT && SPI_processed > 0) {
+			char *tmp;
+
+			tupdesc = SPI_tuptable->tupdesc;
+			tuptable = SPI_tuptable;
+			tuple = tuptable->vals[0];
+
+			strcpy(values[4], SPI_getvalue(tuple, tupdesc, 8));
+			tmp = SPI_getvalue(tuple, tupdesc, 10);
+			if (tmp != NULL)
+				strcpy(values[5], tmp);
+			tmp = SPI_getvalue(tuple, tupdesc, 14);
+			if (tmp != NULL)
+				strcpy(values[6], tmp);
+			tmp = SPI_getvalue(tuple, tupdesc, 18);
+			if (tmp != NULL)
+				strcpy(values[7], tmp);
+			tmp = SPI_getvalue(tuple, tupdesc, 9);
+			if (tmp != NULL)
+				strcpy(values[8], tmp);
+			tmp = SPI_getvalue(tuple, tupdesc, 13);
+			if (tmp != NULL)
+				strcpy(values[9], tmp);
+			tmp = SPI_getvalue(tuple, tupdesc, 17);
+			if (tmp != NULL)
+				strcpy(values[10], tmp);
+			strcpy(values[11], SPI_getvalue(tuple, tupdesc, 7));
+			tmp = SPI_getvalue(tuple, tupdesc, 21);
+			if (tmp != NULL)
+				strcpy(values[12], tmp);
+			tmp = SPI_getvalue(tuple, tupdesc, 22);
+			if (tmp != NULL)
+				strcpy(values[13], tmp);
+			tmp = SPI_getvalue(tuple, tupdesc, 12);
+			if (tmp != NULL)
+				strcpy(values[14], tmp);
+			tmp = SPI_getvalue(tuple, tupdesc, 16);
+			if (tmp != NULL)
+				strcpy(values[15], tmp);
+			tmp = SPI_getvalue(tuple, tupdesc, 20);
+			if (tmp != NULL)
+				strcpy(values[16], tmp);
+			strcpy(values[17], SPI_getvalue(tuple, tupdesc, 3));
+			tmp = SPI_getvalue(tuple, tupdesc, 5);
+			if (tmp != NULL)
+				strcpy(values[18], tmp);
+			strcpy(values[19], SPI_getvalue(tuple, tupdesc, 2));
+			tmp = SPI_getvalue(tuple, tupdesc, 11);
+			if (tmp != NULL)
+				strcpy(values[20], tmp);
+			tmp = SPI_getvalue(tuple, tupdesc, 15);
+			if (tmp != NULL)
+				strcpy(values[21], tmp);
+			tmp = SPI_getvalue(tuple, tupdesc, 19);
+			if (tmp != NULL)
+				strcpy(values[22], tmp);
+			tmp = SPI_getvalue(tuple, tupdesc, 4);
+			if (tmp != NULL)
+				strcpy(values[23], tmp);
+		} else {
+			elog(NOTICE, "ERROR: CPF1_2 failed.");
+		}
+
+		sprintf(sql, CPF1_3, cust_id);
+#ifdef DEBUG
+		elog(NOTICE, "SQL\n%s", sql);
+#endif /* DEBUG */
+		ret = SPI_exec(sql, 0);
+		sprintf(values[2], "%d", SPI_processed);
+#ifdef DEBUG
+		elog(NOTICE, "%d row(s) returned.", SPI_processed);
+#endif /* DEBUG */
+		if (ret == SPI_OK_SELECT && SPI_processed > 0) {
+			tupdesc = SPI_tuptable->tupdesc;
+			tuptable = SPI_tuptable;
+			tuple = tuptable->vals[0];
+
+			strcpy(values[1], "{");
+			strcpy(values[3], "{");
+
+			if (SPI_processed > 0) {
+				strcat(values[1], SPI_getvalue(tuple, tupdesc, 1));
+				strcat(values[3], SPI_getvalue(tuple, tupdesc, 3));
+			}
+			for (i = 1; i < SPI_processed; i++) {
+				tuple = tuptable->vals[i];
+				strcat(values[1], ",");
+				strcat(values[1], SPI_getvalue(tuple, tupdesc, 1));
+
+				strcat(values[3], ",");
+				strcat(values[3], SPI_getvalue(tuple, tupdesc, 3));
+			}
+			strcat(values[1], "}");
+			strcat(values[3], "}");
+		} else {
+			elog(NOTICE, "ERROR: CPF1_3 failed.");
+		}
+
+		/* Total number of tuples to be returned. */
+		funcctx->max_calls = 1;
+
+		/* Build a tuple descriptor for our result type. */
+		if (get_call_result_type(fcinfo, NULL, &tupdesc) !=
+				TYPEFUNC_COMPOSITE) {
+			ereport(ERROR,
+					(errcode(ERRCODE_FEATURE_NOT_SUPPORTED),
+					errmsg("function returning record called in context "
+							"that cannot accept type record")));
+		}
+
+		/*
+		 * Generate attribute metadata needed later to produce tuples from raw
+		 * C strings.
+		 */
+		funcctx->attinmeta = TupleDescGetAttInMetadata(tupdesc);
+
+		/* save SPI data for use across calls */
+		funcctx->user_fctx = tuptable;
+
+		MemoryContextSwitchTo(oldcontext);
+	}
+
+	/* Stuff done on every call of the function. */
+	funcctx = SRF_PERCALL_SETUP();
+
+	if (funcctx->call_cntr < funcctx->max_calls) {
+		Datum result;
+		HeapTuple tuple;
+
+		snprintf(values[0], 12, "%ld", cust_id);
+
+		/* Build a tuple. */
+		tuple = BuildTupleFromCStrings(funcctx->attinmeta, values);
+
+		/* Make the tuple into a datum. */
+		result = HeapTupleGetDatum(tuple);
+
+		/* Free memory. */
+		for (i = 0; i < 24; i++) {
+			pfree(values[i]);
+		}
+		pfree(values);
+
+		SRF_RETURN_NEXT(funcctx, result);
+	} else {
+		SPI_finish();
+		SRF_RETURN_DONE(funcctx);
+	}
+}
+
+PG_FUNCTION_INFO_V1(CustomerPositionFrame2);
+
+Datum CustomerPositionFrame2(PG_FUNCTION_ARGS)
+{
+	FuncCallContext *funcctx;
+	char **values = NULL;
+	int i;
+
+	if (SRF_IS_FIRSTCALL()) {
+		MemoryContext oldcontext;
+
+		TupleDesc tupdesc;
+		SPITupleTable *tuptable = NULL;
+		HeapTuple tuple;
+
+		int64 acct_id;
+
+		int ret;
+		char sql[1024];
+
+		acct_id = PG_GETARG_INT64(0);
+#ifdef DEBUG
+		elog(NOTICE, "[1] %ld", acct_id);
+#endif /* DEBUG */
+
+		/*
+		 * Prepare a values array for building the returned tuple.
+		 * This should be an array of C strings, which will
+		 * be processed later by the type input functions.
+		 */
+		values = (char **) palloc(sizeof(char *) * 7);
+		values[0] = (char *) palloc(602 * sizeof(char));
+		values[1] = (char *) palloc(3 * sizeof(char));
+		values[2] = (char *) palloc(202 * sizeof(char));
+		values[3] = (char *) palloc(2 * sizeof(char));
+		values[4] = (char *) palloc(472 * sizeof(char));
+		values[5] = (char *) palloc(472 * sizeof(char));
+		values[6] = (char *) palloc(322 * sizeof(char));
+
+		/*
+		 * I think this is the fastest way to initialize the strings, as
+		 * opposed to zeroing out the entire chunk of memory since palloc()
+		 * doesn't zero out the memory for us.
+		 */
+		for (i = 0; i < 7; i++) {
+			values[i][0] = '\0';
+		}
+
+		/* Create a function context for cross-call persistence. */
+		funcctx = SRF_FIRSTCALL_INIT();
+
+		/* Switch to memory context appropriate for multiple function calls. */
+		oldcontext = MemoryContextSwitchTo(funcctx->multi_call_memory_ctx);
+
+		SPI_connect();
+
+		sprintf(sql, CPF2_1, acct_id);
+#ifdef DEBUG
+		elog(NOTICE, "SQL\n%s", sql);
+#endif /* DEBUG */
+		ret = SPI_exec(sql, 0);
+		sprintf(values[1], "%d", SPI_processed);
+#ifdef DEBUG
+		elog(NOTICE, "%d row(s) returned.", SPI_processed);
+#endif /* DEBUG */
+		if (ret == SPI_OK_SELECT && SPI_processed > 0) {
+			tupdesc = SPI_tuptable->tupdesc;
+			tuptable = SPI_tuptable;
+			tuple = tuptable->vals[0];
+
+			/* FIXME: How is 'status' supposed to be set? */
+			strcpy(values[3], "1");
+
+			strcpy(values[0], "{");
+			strcpy(values[2], "{");
+			strcpy(values[4], "{");
+			strcpy(values[5], "{");
+			strcpy(values[6], "{");
+			if (SPI_processed > 0) {
+				strcat(values[0], SPI_getvalue(tuple, tupdesc, 5));
+				strcat(values[2], SPI_getvalue(tuple, tupdesc, 3));
+				strcat(values[4], SPI_getvalue(tuple, tupdesc, 2));
+				strcat(values[5], SPI_getvalue(tuple, tupdesc, 1));
+				strcat(values[6], SPI_getvalue(tuple, tupdesc, 4));
+			}
+			for (i = 1; i < SPI_processed; i++) {
+				tuple = tuptable->vals[i];
+
+				strcat(values[0], ",");
+				strcat(values[0], SPI_getvalue(tuple, tupdesc, 5));
+
+				strcat(values[2], ",");
+				strcat(values[2], SPI_getvalue(tuple, tupdesc, 3));
+
+				strcat(values[4], ",");
+				strcat(values[4], SPI_getvalue(tuple, tupdesc, 2));
+
+				strcat(values[5], ",");
+				strcat(values[5], SPI_getvalue(tuple, tupdesc, 1));
+
+				strcat(values[6], ",");
+				strcat(values[6], SPI_getvalue(tuple, tupdesc, 4));
+			}
+			strcat(values[0], "}");
+			strcat(values[2], "}");
+			strcat(values[4], "}");
+			strcat(values[5], "}");
+			strcat(values[6], "}");
+		} else {
+			elog(NOTICE, "ERROR: CPF2_1 failed.");
+		}
+
+		/* Total number of tuples to be returned. */
+		funcctx->max_calls = 1;
+
+		/* Build a tuple descriptor for our result type. */
+		if (get_call_result_type(fcinfo, NULL, &tupdesc) !=
+				TYPEFUNC_COMPOSITE) {
+			ereport(ERROR,
+					(errcode(ERRCODE_FEATURE_NOT_SUPPORTED),
+					errmsg("function returning record called in context "
+							"that cannot accept type record")));
+		}
+
+		/*
+		 * Generate attribute metadata needed later to produce tuples from raw
+		 * C strings.
+		 */
+		funcctx->attinmeta = TupleDescGetAttInMetadata(tupdesc);
+
+		/* save SPI data for use across calls */
+		funcctx->user_fctx = tuptable;
+
+		MemoryContextSwitchTo(oldcontext);
+	}
+
+	/* Stuff done on every call of the function. */
+	funcctx = SRF_PERCALL_SETUP();
+
+	if (funcctx->call_cntr < funcctx->max_calls) {
+		Datum result;
+		HeapTuple tuple;
+
+#ifdef DEBUG
+		for (i = 0; i < 7; i++) {
+			elog(NOTICE, "%d %s", i, values[i]);
+		}
+#endif /* DEBUG */
+
+		/* Build a tuple. */
+		tuple = BuildTupleFromCStrings(funcctx->attinmeta, values);
+
+		/* Make the tuple into a datum. */
+		result = HeapTupleGetDatum(tuple);
+
+		/* Free memory. */
+		for (i = 0; i < 7; i++) {
+			pfree(values[i]);
+		}
+		pfree(values);
+		SRF_RETURN_NEXT(funcctx, result);
+	} else {
+		SPI_finish();
+		SRF_RETURN_DONE(funcctx);
+	}
+}
