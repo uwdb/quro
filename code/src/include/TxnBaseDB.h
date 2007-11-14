aebd4d0855a469eb96612a44a8d226d9a6874e41
markwkm <devnull@localhost>
2007-11-14 06:19:46 +0000

    [project @ 1904] Fixed the Tokenizer function to not do anything is the string length matches the empty array.  i.e. '{}'.  Also fixed the check to no use string::npos to determine the end of the string, but the actual size of the string being passed into the function.

--- code/src/include/TxnBaseDB.h
+++ code/src/include/TxnBaseDB.h
@@ -29,19 +29,26 @@ public:
 	void BeginTxn();
 	void CommitTxn();
 	void RollbackTxn();
-
 };
 
 // String Tokenizer
-void inline Tokenize(const string& str, vector<string>& tokens,
-		const string& delimiters = "|")
+void inline Tokenize(const string& str2, vector<string>& tokens,
+		const string& delimiters = ",")
 {
+	// This is essential an empty array. i.e. '{}'
+	if (str2.size() < 3)
+		return;
+
+	// We only call this function because we need to chop up arrays that
+	// are in the format '{1,2,3,...}', so trim off the braces.
+	string str = str2.substr(1, str2.size() - 1);
+
 	// Skip delimiters at beginning.
 	string::size_type lastPos = str.find_first_not_of(delimiters, 0);
 	// Find first "non-delimiter".
 	string::size_type pos     = str.find_first_of(delimiters, lastPos);
 	
-	while (string::npos != pos || string::npos != lastPos)
+	while (pos < str2.size())
 	{
 		// Found a token, add it to the vector.
 		tokens.push_back(str.substr(lastPos, pos - lastPos));
@@ -51,8 +58,6 @@ void inline Tokenize(const string& str, vector<string>& tokens,
 		pos = str.find_first_of(delimiters, lastPos);
 	}
 }
-
-
 
 }	// namespace TPCE
 
